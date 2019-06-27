545a0c8bd670ee756198fc027014f04c0056206c
levon <levon.gambaryan@gmail.com>
2019-06-28 00:55:36 +0300

    add kostya-lazy branch

--- src/js/radio.js
+++ src/js/radio.js
@@ -1,44 +1,44 @@
 class Radio {
-    constructor() {
-        this._initRadio();
-    }
+  constructor() {
+    this._initRadio();
+  }
 
-    play() {
-        // or stop
-    }
-    next() {}
+  play() {
+    // or stop
+  }
+  next() {}
 
-    _buildPlaylist() {
-        // randomly
-    }
+  _buildPlaylist() {
+    // randomly
+  }
 
-    _initRadio() {
-        console.log(222);
-        this._getSongsList().then((result) => {
-            console.log(333);
-            var html = '<div class="radio">' + this._parseTracks(result.songs) + '</div>';
-            console.log(12, html);
-            console.log(13, Howl, Howler, HowlerGlobal);
-        });
-    }
-    _getSongsList() {
-        return fetch('/audio/songs.json').then((response) => {
-            console.log(21, response);
-            return response.json();
-        }).catch((err) => {
-            console.error('something gone wrong with request', err);
-        });
-    }
-    _parseTracks(tracks) {
-        // tracks is ({ title: String, url: String, album: String, year: Number })[]
-        var html = [];
+  _initRadio() {
+    console.log(222);
+    this._getSongsList().then((result) => {
+      console.log(333);
+      var html = '<div class="radio">' + this._parseTracks(result.songs) + '</div>';
+      console.log(12, html);
+      console.log(13, Howl, Howler, HowlerGlobal);
+    });
+  }
+  _getSongsList() {
+    return fetch('/audio/songs.json').then((response) => {
+      console.log(21, response);
+      return response.json();
+    }).catch((err) => {
+      console.error('something gone wrong with request', err);
+    });
+  }
+  _parseTracks(tracks) {
+    // tracks is ({ title: String, url: String, album: String, year: Number })[]
+    var html = [];
 
-        html.push('<div class="radio__list">');
-        tracks.forEach(v => html.push('<a href="' + v.url + '" target="_blank">' + v.title + '</a>'));
-        html.push('</div>');
+    html.push('<div class="radio__list">');
+    tracks.forEach(v => html.push('<a href="' + v.url + '" target="_blank">' + v.title + '</a>'));
+    html.push('</div>');
 
-        return html.join('');
-    }
+    return html.join('');
+  }
 }
 
 export default Radio;
